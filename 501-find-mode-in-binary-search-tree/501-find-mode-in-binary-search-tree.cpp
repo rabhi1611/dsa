a84e2cab74e6610a9577f308aeb009e344de51c6
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2022-01-20 13:45:18 +0530

    Time: 32 ms (26.73%), Space: 23.8 MB (96.46%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
@@ -11,26 +11,47 @@
  */
 class Solution {
 public:
-    unordered_map<int,int>m;
+    
+    int fun2(TreeNode*& root,int target){
+        if(root==NULL){
+            return 0;
+        }
+        int a=0;
+        int b=0;
+        a=fun2(root->left,target);
+        b=fun2(root->right,target);
+        if(root->val==target){
+            return a+b+1;
+        }
+        return a+b;
+    }
+    
+    
+    //unordered_map<int,int>m;
     int max_count=0;
+    vector<int>ans;
     void fun(TreeNode*& root){
         if(root==NULL){
             return;
         }
-        m[root->val]++;
-        max_count=max(max_count,m[root->val]);
+        int c=fun2(root,root->val);
+        //cout<<c<<endl;
+        if(c>max_count){
+            ans.clear();
+            ans.push_back(root->val);
+            max_count=c;
+        }
+        else if(c==max_count){
+            ans.push_back(root->val);
+        }
+        //m[root->val]++;
+        //max_count=max(max_count,m[root->val]);
         fun(root->left);
         fun(root->right);
         return;
     }
     vector<int> findMode(TreeNode* root) {
         fun(root);
-        vector<int>ans;
-        for(auto x:m){
-            if(x.second==max_count){
-                ans.push_back(x.first);
-            }
-        }
         return ans;
     }
 };
