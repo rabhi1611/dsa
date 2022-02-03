bac29cf4e6bd278808bf099392679201854e82ab
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2022-02-03 09:40:22 +0530

    Time: 608 ms (13.60%), Space: 33.8 MB (12.02%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,38 +1,50 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-       vector<vector<int>> ans;
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        for(int i=0;i<n; i++)
-        {
-            int j=i+1,k=n-1;//two pointers
-            while(j<n && j<k)
-            {
-                if(nums[j]+nums[k] == -nums[i])
-                {
-                    ans.push_back({nums[i],nums[j],nums[k]});
-                    while(k!=0 && nums[k]==nums[k-1]) k--;//to avoid duplicates 
-                    while(j!=n-1 && nums[j]==nums[j+1]) j++;
-                    j++,k--;
-                }
-                else if(nums[j]+nums[k] > -nums[i]) 
-                {
-                    while(k!=0 && nums[k]==nums[k-1]) k--;
+        sort(nums.begin(), nums.end());
+        set<vector<int>>s;
+        for(int i=0; i<nums.size() ;i++){
+            int j = i+1;
+            int k = nums.size()-1;
+            while(j < k){
+                if(nums[j] + nums[k] == (-1 * nums[i])){
+                    vector<int>temp;
+                    temp.push_back(nums[i]);
+                    temp.push_back(nums[j]);
+                    temp.push_back(nums[k]);
+                    sort(temp.begin(), temp.end());
+                    if(s.count(temp) > 0){
+                        
+                    }else{
+                        s.insert(temp);
+                    }
+                    while(k-1>=0 && nums[k-1] == nums[k]){
+                        k--;
+                    }
+                    while(j+1<nums.size() && nums[j+1] == nums[j]){
+                        j++;
+                    }
+                    j++;
                     k--;
-                }
-                else
-                {
-                    while(j!=n-1 && nums[j]==nums[j+1]) j++;
+                }else if(nums[j] + nums[k] > (-1 * nums[i])){
+                    while(k-1>=0 && nums[k-1] == nums[k]){
+                        k--;
+                    }
+                    k--;
+                }else{
+                    while(j+1<nums.size() && nums[j+1] == nums[j]){
+                        j++;
+                    }
                     j++;
                 }
             }
-            while(i!=n-1 && nums[i]==nums[i+1]) i++;
         }
-        for(auto triplet : ans)
-            sort(triplet.begin(),triplet.end());
+        vector<vector<int>> ans;
+        for(auto x:s){
+            //cout<<x[0]<<" "<<x[1]<<" "<<x[2]<<endl;
+            //cout<<nums[x[0]]<<" "<<nums[x[1]]<<" "<<nums[x[2]]<<endl;
+            ans.push_back(x);
+        }
         return ans;
-}
-
-
+    }
 };
