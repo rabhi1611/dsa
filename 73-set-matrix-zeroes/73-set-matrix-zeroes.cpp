31048ee22789e1ae673edc40daece1cf76057ab0
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2022-03-29 21:18:14 +0530

    Time: 20 ms (52.13%), Space: 13.4 MB (11.14%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     
-    void fun(vector<vector<int>>& matrix, vector<vector<int>>& t_grid){
+    void fun(vector<vector<int>>& matrix){
+        
+        unordered_set<int> s1;
+        unordered_set<int> s2;
+        
         for(int i = 0; i < matrix.size(); i++){
             for(int j = 0; j < matrix[i].size(); j++){
                 if(matrix[i][j] == 0){
-                    for(int k = 0; k < matrix[i].size(); k++){
-                        t_grid[i][k] = 0;
-                    }
-                    for(int k = 0; k < matrix.size(); k++){
-                        t_grid[k][j] = 0;
-                    }
+                    s1.insert(i);
+                    s2.insert(j);
+                }
+            }
+        }
+        
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[i].size(); j++){
+                if(s1.count(i) != 0 || s2.count(j) != 0){
+                    matrix[i][j] = 0;
                 }
             }
         }
     }
     
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>> t_grid(matrix.size(), vector<int>(matrix[0].size(), 0));
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[i].size(); j++){
-                t_grid[i][j] = matrix[i][j];
-            }
-        }
-        fun(matrix, t_grid);
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[i].size(); j++){
-                matrix[i][j] = t_grid[i][j];
-            }
-        }
+        fun(matrix);
     }
 };
