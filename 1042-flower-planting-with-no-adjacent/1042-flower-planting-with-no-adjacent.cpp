f50b3940312ca3cf1c7d3679e7f841f85d488812
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2022-04-28 13:55:54 +0530

    Time: 201 ms (47.41%), Space: 72.9 MB (9.95%) - LeetHub

--- 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
+++ 1042-flower-planting-with-no-adjacent/1042-flower-planting-with-no-adjacent.cpp
@@ -1,34 +1,50 @@
 class Solution {
 public:
     
-    vector<int> colorGraph(int n, unordered_map<int, list<int>>& g) {
-        // -1 indicates uncolored
-        vector<int> color(n, -1);
-        for(int i = 0; i < n; i++) {
-            // To track the colors used by neigbors
-            vector<bool> color_used(4, false);
-            // mark all used colors
-            for(const auto& v: g[i])
-                if(color[v] != -1)
-                    color_used[color[v]-1] = true;
-            // look for the unused color
-            for(int c = 0; c < 4; c++)
-                if(color_used[c] == false) {
-                    color[i] = c + 1;
-                    break;
-                }
-        }
-        return color;
-    }
     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
         
         unordered_map<int, list<int>> mp;
         
         for(auto x: paths){
-            mp[x[0] - 1].push_back(x[1] - 1);
-            mp[x[1] - 1].push_back(x[0] - 1);
+            mp[x[0]].push_back(x[1]);
+            mp[x[1]].push_back(x[0]);
         }
         
-        return colorGraph(n, mp);
+        vector<int>color(n + 1, 0);
+        vector<int>visited(n + 1, 0);
+        
+        for(int i = 1; i <= n; i++){
+            if(visited[i] == 0){
+                queue<int> q;
+
+                q.push(i);
+                color[i] = 1;
+
+
+                while(!q.empty()){
+                    
+                    int size = q.size();
+                    while(size--){
+                        int node = q.front();
+                        q.pop(); 
+                        visited[node] = 1;
+                        for(auto x: mp[node]){
+                            if(color[x] == 0){
+                                color[x] = (color[node]) % 4 + 1;
+                                q.push(x);
+                                continue;
+                            }
+                            if(color[x] == color[node]){
+                                color[x] = (color[x]) % 4 + 1;
+                                continue;
+                            }
+                        }
+                    }
+                }
+                
+            }
+        }
+        vector<int>ans = {color.begin() +  1, color.end()};
+        return ans;
     }
 };
