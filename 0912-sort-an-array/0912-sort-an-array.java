85a61d8a0dafaacda5fa0376d072f1892589f179
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2024-12-23 11:10:25 +0530

    Time: 29 ms (39.96%), Space: 57 MB (25.58%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,59 @@
+class Solution {
+    
+    private void sort(int arr[], int l, int r){
+        
+        if(l >= r){
+            return;
+        }
+        
+        int mid = (l + r) / 2;
+        
+        sort(arr, l, mid);
+        sort(arr, mid + 1, r);
+        
+        int i = l;
+        int j = mid + 1;
+        int n = mid;
+        int m = r;
+        
+        int[] subSortedArray = new int[(n - i + 1) + (m - j + 1)];    
+    
+        int k = 0;
+        while(i <= n && j <= m){
+            if(arr[i] <= arr[j]){
+                subSortedArray[k] = arr[i];
+                i++;
+            } else{
+                subSortedArray[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        
+        while(i <= n){
+            subSortedArray[k] = arr[i];
+            i++;
+            k++;
+        }
+        
+        while(j <= m){
+            subSortedArray[k] = arr[j];
+            j++;
+            k++;
+        }
+            
+        k = l;
+        i = 0;
+        while(k <= r){
+            arr[k] = subSortedArray[i];
+            k++;
+            i++;
+        }
+        return;
+    }
+    
+    public int[] sortArray(int[] nums) {
+        sort(nums, 0, nums.length - 1);
+        return nums;
+    }
+}
