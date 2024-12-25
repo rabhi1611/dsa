3c865794af67c2519e87823f068bbee077b9347f
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2024-12-25 10:15:21 +0530

    Time: 1 ms (98.79%), Space: 41.8 MB (90.34%) - LeetHub

---
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int secondHighest(String s) {
+        char slar = 'a';
+        char lar = 'a';
+        
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            
+            if((c >= 48 && c <= 57) && lar == 'a'){
+                lar = c;
+                continue;
+            }
+            
+            if((c >= 48 && c <= 57) && c > lar){
+                slar = lar;
+                lar = c;
+            }
+            
+            if((c >= 48 && c <= 57) && c < lar){
+                if(slar == 'a'){
+                    slar = c;
+                }
+                
+                if(c > slar){
+                    slar = c;
+                }
+            }
+        }
+       
+        return slar == 'a' ? -1 : slar -  48;
+    }
+}
