d16d97d293556a612b115034186c39cabf5dc983
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2025-03-10 20:47:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.java
@@ -0,0 +1,74 @@
+class Solution {
+    Map<List<Integer>, String> mp = new LinkedHashMap<>();
+
+    public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
+        int k = indices.length;
+        StringBuilder ans = new StringBuilder();
+
+        for(int i = 0; i < k; i++){
+            int idx = indices[i];
+            String src = sources[i];
+            String tgt = targets[i];
+
+            // isSrcPresent?
+            if(isSrcPresent(s, idx, src)){
+                // put src
+                createRecord(idx, src, tgt);
+            }
+        }
+
+        System.out.println(mp);
+
+        AtomicInteger peIdx = new AtomicInteger(0);
+        mp.entrySet().forEach(entry -> {
+            int si = entry.getKey().get(0);
+            int ei = entry.getKey().get(1);
+
+            String val = entry.getValue();
+
+            for(int i = peIdx.get(); i < si; i++){
+                ans.append(s.charAt(i));
+            }
+
+            ans.append(val);
+            
+            peIdx.set(ei + 1);
+        });
+
+        for(int i = peIdx.get(); i < s.length(); i++){
+            ans.append(s.charAt(i));
+        }
+
+        return ans.toString();
+    }
+
+
+    private void createRecord(int sIdx, String src, String tgt){
+        mp.put(Arrays.asList(sIdx, sIdx + src.length() - 1), tgt);
+    }
+
+
+    private boolean isSrcPresent(String s, int sIdx, String src){
+        int i = sIdx;
+        int j = 0;
+
+        while(i < s.length() && j < src.length()){
+            if(s.charAt(i) != src.charAt(j)){
+                return false;
+            }
+
+            i += 1;
+            j += 1;
+        }
+
+        if(i == s.length() && j == src.length()){
+            return true;
+        }
+
+        if(i == s.length()){
+            return false;
+        }
+
+        return true;
+    }
+}
