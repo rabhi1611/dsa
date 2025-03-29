a9c109f5f9e3c871b6886aebc817e79dd6fa5a91
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2025-03-29 18:07:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,10 +1,12 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        return helper(nums, 0, n, target);
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return helper(nums, 0, n, target, dp);
     }
 
-    private int helper(int[] nums, int i, int n, int target){
+    private int helper(int[] nums, int i, int n, int target, int[] dp){
         
         if(i == n){
             if(target == 0){
@@ -13,9 +15,11 @@ class Solution {
             return 0;
         }
 
-        int subSum1 = helper(nums, i + 1, n, target - nums[i]);
-        int subSum2 = helper(nums, i + 1, n, target + nums[i]);
+        if(dp[i] != -1) return dp[i]; 
 
-        return subSum1 + subSum2;
+        int subSum1 = helper(nums, i + 1, n, target - nums[i], dp);
+        int subSum2 = helper(nums, i + 1, n, target + nums[i], dp);
+
+        return dp[i] = subSum1 + subSum2;
     }
 }
