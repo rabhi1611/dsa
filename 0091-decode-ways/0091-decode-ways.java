028e9ffc47406746c8d1c10d4577b3975b730ff2
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2025-03-30 18:20:18 +0530

    Time: 6 ms (5.45%), Space: 42.7 MB (5.14%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,34 +1,31 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        return helper(0, n, s);
+        int[] dp = new int[102];
+        Arrays.fill(dp, -1);
+        return helper(0, n, s, dp);
     }
 
 
-    private int helper(int i, int n, String s){
+    private int helper(int i, int n, String s, int[] dp){
         if(i >= n){
             return 1;
         }
-        
-        /*if(i == n - 1){
-            if((s.charAt(i) - 48) >= 1 && (s.charAt(i) - 57) <= 9){
-                return 1;
-            }
-            return 0;
-        }*/
+
+        if(dp[i] != -1) return dp[i];
 
         int sa1 = -1, sa2 = -1;
 
         String t =  "" + s.charAt(i);
 
         if(Integer.valueOf(t) >= 1 && Integer.valueOf(t) <= 9){
-            sa1 = helper(i + 1, n, s);
+            sa1 = helper(i + 1, n, s, dp);
         }
 
         if(i + 1 < n){
             t += s.charAt(i + 1); 
             if(Integer.valueOf(t) >= 10 && Integer.valueOf(t) <= 26){
-                sa2 = helper(i + 2, n, s);
+                sa2 = helper(i + 2, n, s, dp);
             }
         }
 
@@ -44,6 +41,6 @@ class Solution {
             return sa1;
         }
 
-        return sa1 + sa2;
+        return dp[i] = sa1 + sa2;
     }
 }
