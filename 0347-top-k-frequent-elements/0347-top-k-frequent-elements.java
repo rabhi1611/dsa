736695fe9d1276aeb4931bcdd658dc999f6ac82f
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2025-04-27 11:55:14 +0530

    Time: 13 ms (76.76%), Space: 48.9 MB (33.52%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -8,13 +8,26 @@ class Solution {
 
         int[] ans = new int[k];
 
-        AtomicInteger i = new AtomicInteger();
+        /*AtomicInteger i = new AtomicInteger();
         mp.entrySet().stream().sorted(Map.Entry.<Integer, Integer> comparingByValue().reversed()).forEach(el -> {
             if(i.get() < k){
                 ans[i.get()] = el.getKey();
                 i.set(i.get() + 1);
             }
-        });
+        });*/
+
+
+        Queue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(b.getValue(), a.getValue())
+        );
+
+        for(Map.Entry<Integer, Integer> e : mp.entrySet()){
+            pq.offer(e);
+        }
+
+        for(int i = 0; i < k; i++){
+            ans[i] = pq.poll().getKey();
+        }
 
         return ans;
     }
