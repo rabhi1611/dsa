6120e38d239b38ec8199d5111afbd609ae027f00
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2025-06-21 18:43:48 +0530

    Time: 1999 ms (5.02%), Space: 46.2 MB (5.08%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.java
+++ 0934-shortest-bridge/0934-shortest-bridge.java
@@ -31,13 +31,13 @@ class Solution {
             }
         }
 
-         // print
+        /* // print
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 System.out.printf("%d ", grid[i][j]);
             }
             System.out.printf("\n");
-        }
+        }*/
         
 
         // find sortest path betwn 2 of them
@@ -50,47 +50,51 @@ class Solution {
                 if(grid[i][j] == 2){
                     continue;
                 }
-
-                //System.out.println(visited[i][j]);
-                int[][] visited2 = new int[n][n];
-                visited2[i][j] = 1;
                 // only 1 island to traverse
-                int sans = sortestPath(i, j, n, grid, visited2);
-                System.out.printf("%d %d %d \n", i, j, sans);
+                int sans = sortestPath(i, j, n, grid);
+                //System.out.printf("%d %d %d \n", i, j, sans);
                 ans = Math.min(ans, sans);
             }
         }
 
-        return ans - 1;
+        return ans;
     }
 
-    private int sortestPath(int i, int j, int n, int[][] grid, int[][] visited){
+    private int sortestPath(int i, int j, int n, int[][] grid){
 
-        if(grid[i][j] == 2){
-            return 0;
-        }
+        int[][] visited = new int[n][n];
+        Queue<List<Integer>> q = new ArrayDeque<>();
 
-        int ans = Integer.MAX_VALUE;
+        q.add(List.of(i, j, 0));
+        visited[i][j] = 1;
 
         int[] rows = new int[]{1, -1, 0, 0};
         int[] cols = new int[]{0, 0, 1, -1};
 
-        for(int k = 0; k < 4; k += 1){
-            int nr = i + rows[k];
-            int nc = j + cols[k];
 
-            if(nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] != 1 && visited[nr][nc] == 0){
-                visited[nr][nc] = 1;
-                ans = Math.min(ans, sortestPath(nr, nc, n, grid, visited));
-                visited[nr][nc] = 0;
+        while(!q.isEmpty()){
+            List<Integer> ls = q.poll();
+
+            int ci = ls.get(0);
+            int cj = ls.get(1);
+            int cs = ls.get(2);
+
+            for(int k = 0; k < 4; k++){
+                int ni = ci + rows[k];
+                int nj = cj + cols[k];
+
+                if(ni >= 0 && nj >= 0 && ni < n && nj < n && visited[ni][nj] == 0){
+                    if(grid[ni][nj] == 2){
+                        return cs;
+                    }
+
+                    visited[ni][nj] = 1;
+                    q.add(List.of(ni, nj, cs + 1));
+                }
             }
         }
 
-        if(ans == Integer.MAX_VALUE){
-            return ans;
-        }
-
-        return 1 + ans;
+        return -1;
     }
 
     private void visit(int i, int j, int n, int[][] grid, int[][] visited, int islandNum){
