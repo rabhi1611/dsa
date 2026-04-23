c0a26c801d35159883a276f2ffaa66629396bf04
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2026-04-23 10:04:00 +0530

    Time: 0 ms (100%), Space: 42.6 MB (61.73%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,57 +1,44 @@
 class MyQueue {
-    
-    Stack<Integer> st = new Stack<>();
+    private final Stack<Integer> st1;
+    private final Stack<Integer> st2;
 
     public MyQueue() {
-        
+        st1 = new Stack<>();
+        st2 = new Stack<>();
     }
     
     public void push(int x) {
-        st.push(x);
+        st1.push(x);
     }
     
     public int pop() {
-        Stack<Integer> tst = new Stack<>();
-        
-        while(!st.isEmpty()){
-            int tempVar = st.pop();
-            tst.push(tempVar);
-            
-        }
-        
-        int ans = tst.pop();
-        
-        while(!tst.isEmpty()){
-            int tempVar = tst.pop();
-            st.push(tempVar);
-            
-        }
-        
+        fill();
+        int ans = st2.pop();
+        unfill();
         return ans;
     }
     
     public int peek() {
-        Stack<Integer> tst = new Stack<>();
-        
-        while(!st.isEmpty()){
-            int tempVar = st.pop();
-            tst.push(tempVar);
-            
-        }
-        
-        int ans = tst.peek();
-        
-        while(!tst.isEmpty()){
-            int tempVar = tst.pop();
-            st.push(tempVar);
-            
-        }
-        
-        return ans;        
+        fill();
+        int ans = st2.peek();
+        unfill();
+        return ans;
     }
     
     public boolean empty() {
-        return st.isEmpty();
+        return st1.isEmpty();
+    }
+
+    private void fill() {
+        while(!st1.isEmpty()){
+            st2.push(st1.pop());
+        }
+    }
+
+    private void unfill(){
+        while(!st2.isEmpty()){
+            st1.push(st2.pop());
+        }
     }
 }
 
