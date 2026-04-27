2364baa4bbc84adf2987ec155cb88a9c8aa9ba0e
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2026-04-27 21:10:44 +0530

    Time: 29 ms (7.49%), Space: 50.9 MB (70.84%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,34 +1,17 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length, m = matrix[0].length;
-        int low = matrix[0][0], high = matrix[n - 1][m - 1];
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
-        while(low < high){
-            int mid = low + (high - low) / 2;
-            int pos = countHelper(matrix, mid, n, m);
+        for(int i = 0; i < matrix.length; i += 1){
+            for(int j = 0; j < matrix[i].length; j += 1){
+                pq.offer(matrix[i][j]);
 
-            if(pos < k){
-                low = mid + 1;
-            } else {
-                high = mid;
+                if(pq.size() > k){
+                    pq.poll();
+                }
             }
         }
 
-        return low;
-    }
-
-    private int countHelper(int[][] matrix, int mid, int n, int m){
-        int cnt = 0, i = 0;
-
-        while(i < n){
-            int j = m - 1;
-            while(j >= 0 && matrix[i][j] > mid){
-                j -= 1;
-            }
-            cnt += (j + 1);
-            i += 1;
-        }       
-
-        return cnt;
+        return pq.peek();
     }
 }
