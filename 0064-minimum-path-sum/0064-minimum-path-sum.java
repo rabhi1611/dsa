0cdcb5103daae63d395ff5e12762119dcc0e1815
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2026-05-24 11:28:06 +0530

    Time: 5 ms (24.94%), Space: 50 MB (83.06%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,38 +3,22 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i += 1){
-            Arrays.fill(dp[i], -1);
+        int dp[][] = new int[m][n];
+
+        for(int i = m - 1; i >= 0; i -= 1){
+            for(int j = n - 1; j >= 0; j -= 1){
+                if(i + 1 >= m && j + 1 >= n){
+                    dp[i][j] = grid[i][j];
+                } else if (i + 1 >= m){
+                    dp[i][j] = grid[i][j] + dp[i][j + 1];
+                } else if (j + 1 >= n){
+                    dp[i][j] = grid[i][j] + dp[i + 1][j];
+                } else {
+                    dp[i][j] = Math.min(grid[i][j] + dp[i + 1][j], grid[i][j] + dp[i][j + 1]);
+                }
+            }
         }
 
-        return helper(0, 0, m - 1, n - 1, grid, dp);
-    }
-
-    private int helper(int currX, int currY, int endX, int endY, int[][] grid, int[][] dp){
-        if(currX > endX || currY > endY){
-            return Integer.MAX_VALUE;
-        }
-
-        if(dp[currX][currY] != -1){
-            return dp[currX][currY];
-        }
-
-        int down = helper(currX + 1, currY, endX, endY, grid, dp);
-        int right = helper(currX, currY + 1, endX, endY, grid, dp);
-
-        if(down == Integer.MAX_VALUE && right == Integer.MAX_VALUE){
-            return dp[currX][currY] = grid[currX][currY];
-        }
-
-        if(down == Integer.MAX_VALUE){
-            return dp[currX][currY] = grid[currX][currY] + right;
-        }
-
-        if(right == Integer.MAX_VALUE){
-            return dp[currX][currY] = grid[currX][currY] + down;
-        }
-
-        return dp[currX][currY] = Math.min(grid[currX][currY] + down, grid[currX][currY] + right);
+        return dp[0][0];        
     }
 }
