34e0f270ff63194c996f3bbacc87a9392c77a717
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2026-05-24 12:16:45 +0530

    Time: 5 ms (24.94%), Space: 50.4 MB (21.9%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -3,22 +3,29 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        int dp[][] = new int[m][n];
+        int dp[] = new int[n];
+        int right = Integer.MAX_VALUE;
 
         for(int i = m - 1; i >= 0; i -= 1){
             for(int j = n - 1; j >= 0; j -= 1){
                 if(i + 1 >= m && j + 1 >= n){
-                    dp[i][j] = grid[i][j];
+                    dp[j] = grid[i][j];
                 } else if (i + 1 >= m){
-                    dp[i][j] = grid[i][j] + dp[i][j + 1];
+                    dp[j] = grid[i][j] + dp[j + 1];
                 } else if (j + 1 >= n){
-                    dp[i][j] = grid[i][j] + dp[i + 1][j];
+                    dp[j] = grid[i][j] + dp[j];
                 } else {
-                    dp[i][j] = Math.min(grid[i][j] + dp[i + 1][j], grid[i][j] + dp[i][j + 1]);
+                    dp[j] = Math.min(grid[i][j] + dp[j], grid[i][j] + dp[j + 1]);
                 }
             }
+
+            /*for(int k = 0; k < n; k += 1){
+                System.out.printf("%d ", dp[k]);
+            }
+            System.out.printf(" - right: %d\n", right);
+            */
         }
 
-        return dp[0][0];        
+        return dp[0];        
     }
 }
