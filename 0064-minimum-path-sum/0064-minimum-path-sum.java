212539537fba0983f017e953afa08f0869fc1e4e
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2026-05-24 12:17:13 +0530

    Time: 6 ms (10.28%), Space: 50.6 MB (10.6%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -4,7 +4,6 @@ class Solution {
         int n = grid[0].length;
 
         int dp[] = new int[n];
-        int right = Integer.MAX_VALUE;
 
         for(int i = m - 1; i >= 0; i -= 1){
             for(int j = n - 1; j >= 0; j -= 1){
@@ -18,12 +17,6 @@ class Solution {
                     dp[j] = Math.min(grid[i][j] + dp[j], grid[i][j] + dp[j + 1]);
                 }
             }
-
-            /*for(int k = 0; k < n; k += 1){
-                System.out.printf("%d ", dp[k]);
-            }
-            System.out.printf(" - right: %d\n", right);
-            */
         }
 
         return dp[0];        
