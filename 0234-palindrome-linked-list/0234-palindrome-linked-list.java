bcd07c3ddc97c500b5ff282ae10e15c89142d92b
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2026-05-29 09:49:02 +0530

    Time: 8 ms (19.98%), Space: 96.5 MB (32.97%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,28 +9,81 @@
  * }
  */
 class Solution {
-    
     public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null){
+        int n = 0;
+        
+        ListNode temp1 = head;
+        while(temp1 != null){
+            n += 1;
+            temp1 = temp1.next;
+        }
+
+        //System.out.println(n);
+
+        if(n == 1){
             return true;
         }
-        
-        ListNode temp = head;
-        Stack<ListNode> st = new Stack<>();
-        
-        while(temp != null){
-            st.push(temp);
-            temp = temp.next;
+
+        int mid = n / 2;
+        //System.out.println(mid);
+
+        temp1 = head;
+        while(mid > 1){
+            temp1 = temp1.next;
+            mid -= 1;
         }
-        
-        while(!st.isEmpty()){
-            if(st.peek().val != head.val){
+
+        ListNode temp3 = temp1;
+
+        if(n % 2 != 0){
+            temp1 = temp1.next.next;
+        } else {
+            temp1 = temp1.next;
+        }
+
+        temp3.next = null;
+
+        //System.out.println(temp1.val);
+        ListNode temp2 = head;
+
+        // reverse temp1;
+        temp1 = reverse(temp1);
+        //System.out.println(temp1.val);
+
+        while(temp1 != null && temp2 != null){
+            if(temp1.val != temp2.val){
                 return false;
             }
-            st.pop();
-            head = head.next;
+            //System.out.printf("%d %d \n", temp1.val, temp2.val);
+            temp1 = temp1.next;
+            temp2 = temp2.next;
         }
-        
-        return true;
+
+        //if(temp1 != null){
+          //  System.out.printf("t1 %d  \n", temp1.val);
+        //}
+
+        //if(temp2 != null){
+          //  System.out.printf("t2 %d  \n", temp2.val);
+        //}
+
+        if(temp1 == null && temp2 == null){
+            return true;
+        }
+
+        return false;
+    }
+
+    private ListNode reverse(ListNode temp1){
+        if(temp1 == null || temp1.next == null){
+            return temp1;
+        }
+
+        ListNode t = temp1.next;
+        ListNode sub = reverse(temp1.next);
+
+        t.next = temp1;
+        temp1.next = null;
+        return sub;
     }
 }
