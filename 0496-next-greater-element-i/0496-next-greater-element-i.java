163cf2ac72555713ae411aec3a87a8aed85e7ce2
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2026-07-24 14:05:51 +0530

    Time: 5 ms (15.81%), Space: 45.8 MB (11.03%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,31 +1,41 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int n = nums2.length;
         Stack<Integer> st = new Stack<>();
-        
-        Map<Integer, Integer> mp = new HashMap<>();
-        
-        for(int j = nums2.length - 1; j >= 0; j--){
-            int avar = nums2[j];
 
-            while(!st.isEmpty() && st.peek() <= avar){
+        Map<Integer, Integer> mp = new HashMap<>();
+        int[] rec = new int[n];
+
+        for(int i = n - 1; i >= 0; i -= 1){
+            while(!st.isEmpty() && nums2[i] >= st.peek()){
                 st.pop();
             }
-            
-            if(st.isEmpty()){
-                st.push(-1);        
+
+            if(!st.isEmpty()){
+                rec[i] = st.peek();
             }
-            
-            int stvar = st.peek();
-            mp.put(avar, stvar);
-            st.push(avar);
+
+            st.push(nums2[i]);
+            mp.put(nums2[i], i);
         }
-        
+
         int[] ans = new int[nums1.length];
-        
-        for(int i = 0; i < ans.length; i++){
-            ans[i] = mp.get(nums1[i]);
+
+        // now we have the next greater record
+        for(int i = 0; i < nums1.length; i += 1){
+            if(!mp.containsKey(nums1[i])){
+                ans[i] = -1;
+                continue;
+            }
+
+            if(rec[mp.get(nums1[i])] == 0){
+                ans[i] = -1;
+                continue;
+            }
+
+            ans[i] = rec[mp.get(nums1[i])];
         }
-        
+
         return ans;
     }
 }
