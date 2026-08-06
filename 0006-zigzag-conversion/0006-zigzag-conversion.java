406b9a68163ae44b28419c56e998d76dd8026f4b
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2026-08-06 23:01:39 +0530

    Time: 2 ms (99.87%), Space: 46.4 MB (77.04%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -11,17 +11,21 @@ class Solution {
         int limit = 2 * (numRows - 1);
 
         for(int i = 0; i < numRows; i += 1){
-            int j = i;
-            while(j < n){
-                sb.append(s.charAt(j));
-                if(i != 0 && i != numRows - 1){
-                    int nidx = (j + limit - (2 * i));
-                    if(nidx < n){
-                        sb.append(s.charAt(nidx));
-                    }
+            if(i == 0 || i == numRows - 1){
+                int j = i;
+                while(j < n){
+                    sb.append(s.charAt(j));
+                    j += limit;
                 }
-
-                j += limit;
+            }else{
+                int j = i;
+                while(j < n){
+                    sb.append(s.charAt(j));
+                    if((j + limit - (2 * i)) < n){
+                        sb.append(s.charAt((j + limit - (2 * i))));
+                    }
+                    j += limit;
+                }
             }
         }
 
