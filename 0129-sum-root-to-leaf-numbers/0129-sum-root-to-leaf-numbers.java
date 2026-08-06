ea642b59b6a4ba91cfab35e0a9c8d13a442b27b3
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2026-08-06 18:42:17 +0530

    Time: 0 ms (100%), Space: 43 MB (50.01%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -18,23 +18,21 @@ class Solution {
     private int sum = 0;
     
     public int sumNumbers(TreeNode root) {
-        helper(root, new StringBuilder());
+        helper(root, 0);
         return sum;
     }
 
-    private void helper(TreeNode root, StringBuilder st){
+    private void helper(TreeNode root,int number){
         if(root.left == null && root.right == null){
-            st.append(root.val);
-            System.out.println(Integer.valueOf(st.toString()));
-            sum += Integer.valueOf(st.toString());
-            st.setLength(st.length() - 1);
+            number = ((number * 10) + root.val);
+            sum += number;
             return;
         }
 
-        st.append(root.val);
-        if(root.left != null)   helper(root.left, st);
-        if(root.right != null)   helper(root.right, st);
-        
-        if(st.length() > 0) st.setLength(st.length() - 1);
+        number = ((number * 10) + root.val);
+        if(root.left != null)   helper(root.left, number);
+        if(root.right != null)   helper(root.right, number);
+
+        return;
     }
 }
