80a3c1cb98b7328bb4d6b8c7d43d2f630bef6659
Rohit Abhishek <60568574+rabhi1611@users.noreply.github.com>
2026-08-11 11:21:50 +0530

    Time: 76 ms (1.91%), Space: 47.2 MB (5.2%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    private List<List<Integer>> ans = new ArrayList<>();
+
+    public List<List<Integer>> permute(int[] nums) {
+        int n = nums.length;
+
+        for(int i = 0; i < n; i += 1){
+            List<Integer> bucket = new ArrayList<>();
+            int[] visited = new int[n];
+
+            bucket.add(nums[i]);
+            visited[i] = 1;
+            compute(i, n - 1, nums, bucket, visited);
+            bucket.removeLast();
+            visited[i] = 0; 
+        }
+
+        return ans;
+    }
+
+    private void compute(int start, int end, int[] nums
+        , List<Integer> bucket, int[] visited){
+        
+        System.out.println(bucket);
+        System.out.println(visited);
+        System.out.printf("%d %d \n", start, end);
+        if(bucket.size() == nums.length){
+            ans.add(new ArrayList<>(bucket));
+            return;
+        }
+
+        for(int i = 0; i < nums.length; i += 1){
+            if(visited[i] == 0){
+                // take
+                bucket.add(nums[i]);
+                visited[i] = 1;
+                compute(i + 1, end, nums, bucket, visited);
+                bucket.removeLast();
+                visited[i] = 0; 
+            }
+        }
+    }
+}
